7dcba97cca2d1393167460afca51d2a0bba81bd8
jachintanujaya <jachintanujaya@gmail.com>
2016-11-23 12:53:17 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Kulkasku</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="icon" href="images/Favicon.png" sizes="16x16 32x32" type="image/png">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  </head>
+  <body>
+    <div id="header">
+      <img src="images/Logo White.png">
+    </div>
+    <div id="content">
+      <div class="col-md-3 nav-bar">
+        <ul>
+          <li>Home</li>
+        </ul>
+      </div>
+      <div class="col-md-9 description">
+      </div>
+    </div>
+    <div id="footer">
+    </div>
+  </body>
+</html>
